a19cd35fdcdf8d08f9e1dcbcc4b032c5b4e4d88e
Sergey Miroshnichenko <ommmirage@gmail.com>
2021-02-11 20:56:55 +0400

    mlx_linux

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 #include "minilibx/mlx.h"
 
 int main() {
-//    void *mlx;
+    void *mlx;
 
     printf("Hello, World!\n");
-//    mlx = mlx_init();
+    mlx = mlx_init();
 //	mlx_new_window(mlx, 1000, 1000, "Title");
 //	mlx_loop(mlx);
     return 0;
