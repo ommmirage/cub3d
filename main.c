1a05ef5cce496dcdd49e5521c876b6a315698584
Sergey Miroshnichenko <ommmirage@gmail.com>
2021-02-13 10:03:49 +0400

    render next frame

--- main.c
+++ main.c
@@ -1,38 +1,70 @@
 #include <stdio.h>
 #include "minilibx_linux/mlx.h"
 
-typedef struct s_data
+typedef struct s_img
 {
 	void	*img;
 	char	*addr;
 	int 	bits_per_pixel;
 	int 	line_length;
 	int 	endian;
-}			t_data;
+}			t_img;
 
-void	pixel_put(t_data *data, int x, int y, int color)
+typedef struct	s_data
+{
+	void	*mlx;
+	void	*win;
+	t_img	*img;
+}				t_data;
+
+void	pixel_put(t_img *img, int x, int y, int color)
 {
 	int 	offset;
 	char	*dst;
 
-	offset = y * data->line_length + x * (data->bits_per_pixel / 8);
-	dst = data->addr + offset;
+	offset = y * img->line_length + x * (img->bits_per_pixel / 8);
+	dst = img->addr + offset;
 	*(unsigned int*)dst = color;
+}
+
+int 	key_hook(int keycode, t_data *mlx)
+{
+	if (keycode == 65307)
+		mlx_destroy_window(mlx->mlx, mlx->win);
+	printf("%d\n", keycode);
+	return (0);
+}
+
+int 	mouse_hook(int button, int x, int y)
+{
+	printf("%d x=%d, y=%d\n", button, x , y);
+	return (0);
+}
+
+int 	render_next_frame(t_data *data)
+{
+	data->img->img = mlx_new_image(data->mlx, 400, 300);
+	data->img->addr = mlx_get_data_addr(data->img->img,
+									 &data->img->bits_per_pixel,
+									 &data->img->line_length,
+									 &data->img->endian);
+	pixel_put(data->img, 5, 5, 0x0000FF00);
+	mlx_put_image_to_window(data->mlx, data->win, data->img->img, 0, 0);
+	return (0);
 }
 
 int		main()
 {
-    void	*mlx;
-    void	*mlx_win;
-    t_data	img;
+	t_data	data;
 
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1000, 1000, "Hello world!");
-	img.img = mlx_new_image(mlx, 1000, 1000);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
-							  &img.line_length, &img.endian);
-	pixel_put(&img, 5, 5, 0x0000FF00);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	data.mlx = mlx_init();
+	data.win = mlx_new_window(data.mlx, 400, 300, "Hello world!");
+
+	mlx_key_hook(data.win, key_hook, &data);
+
+	mlx_hook(data.win, 04, 1L << 2, mouse_hook, data.mlx);
+
+	mlx_loop_hook(data.mlx, render_next_frame, &data);
+	mlx_loop(data.mlx);
 }
 
